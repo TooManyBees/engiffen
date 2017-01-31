170be63b8329268edb794f086fb4de9e2cc390fc
Jesse Bees <jesse@toomanybees.com>
2017-01-31 01:41:14 -0500

    Use color_quant for real life palette compression :)

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # engiffen
 
-Generates gifs from image files.
+Generates gifs from image sequences.
+
+![source bitmap](tests/ball/ball01.bmp)
+![engiffenned gif](tests/ball.gif)
+![photoshopped gif](tests/ball_ps.gif)
+
+_Source frame, generated gif, and a gif from Photoshop_
 
 ## Minor work to do
 
@@ -12,20 +18,6 @@ Generates gifs from image files.
 * Incremental frame processing
 
   Accept a stream of frames from `stdin` to process individually as they arrive. Put off sorting the final palette and compiling the gif until finished.
-
-* Handle sources with more than 256 colors total
-
-  Naiive implementation is to rank colors based on their frequency in all frames, then adding the
-  256 most frequently used colors to the palette. After palette is filled, for each new color find
-  the closest matching color in the palette and use it instead.
-
-  This is what we currently do, and it has serious limitations on true-color gifs, because it does
-  not determine which colors are important and which are insignificant. Compare the first source frame
-  with the `engiffen`ned output, and output produced by Photoshop.
-
-  ![source bitmap](tests/ball/ball01.bmp)
-  ![engiffenned gif](tests/ball.gif)
-  ![photoshopped gif](tests/ball_ps.gif)
 
 ## Anything else?
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,25 @@
 extern crate image;
-extern crate lab;
 extern crate gif;
+extern crate color_quant;
 
-use std::collections::HashMap;
 use std::io::Write;
 use std::borrow::Cow;
-use image::{GenericImage, DynamicImage, Pixel, Rgba};
+use image::{GenericImage, DynamicImage};
 use gif::{Frame, Encoder, Repeat, SetParameter};
-use lab::Lab;
+use color_quant::NeuQuant;
 
 pub fn engiffen<W: Write>(imgs: &[DynamicImage], mut out: &mut W) {
     let gif_descriptor = palettize(&imgs);
 
-    assert!(gif_descriptor.palette.len() <= 256, "Computed palette has more than 256 colors");
-
     let mut color_map: &mut [u8; 256*3] = &mut [0; 256*3];
-    for (color, idx) in gif_descriptor.palette.iter() {
-        color_map[(*idx as usize)*3] = color.data[0];
-        color_map[(*idx as usize)*3+1] = color.data[1];
-        color_map[(*idx as usize)*3+2] = color.data[2];
+    let mut transparency = None;
+    for (idx, color) in gif_descriptor.palette.chunks(4).enumerate() {
+        color_map[(idx as usize)*3] = color[0];
+        color_map[(idx as usize)*3+1] = color[1];
+        color_map[(idx as usize)*3+2] = color[2];
+        if color[3] == 0 {
+            transparency = Some(idx as u8);
+        }
     }
     let width = gif_descriptor.width;
     let height = gif_descriptor.height;
@@ -29,98 +30,54 @@ pub fn engiffen<W: Write>(imgs: &[DynamicImage], mut out: &mut W) {
         frame.width = width;
         frame.height = height;
         frame.buffer = Cow::Borrowed(&*img);
+        frame.transparent = transparency;
         encoder.write_frame(&frame).unwrap();
     }
 }
 
 struct GifDescriptor {
-    palette: HashMap<Rgba<u8>, u8>,
+    palette: Vec<u8>,
     width: u16,
     height: u16,
-    images: Vec<Vec<u8>>
+    images: Vec<Vec<u8>>,
 }
 
 fn palettize(imgs: &[DynamicImage]) -> GifDescriptor {
     if imgs.is_empty() {
         panic!("No images sent for palettization!");
     }
-    let mut counter: HashMap<Rgba<u8>, usize> = HashMap::new();
     let (width, height) = {
         let img = imgs.iter().nth(0).unwrap();
-        (img.width() as u16, img.height() as u16)
+        (img.width(), img.height())
     };
+    let mut colors: Vec<u8> = Vec::with_capacity(width as usize * height as usize * imgs.len());
     for img in imgs {
-        count_colors(&img, &mut counter);
+        for (_, _, px) in img.pixels() {
+            if px.data[3] == 0 {
+                colors.push(0);
+                colors.push(0);
+                colors.push(0);
+                colors.push(0);
+            } else {
+                colors.push(px.data[0]);
+                colors.push(px.data[1]);
+                colors.push(px.data[2]);
+                colors.push(255);
+            }
+        }
     }
-    let (palette, compressed) = compress_palette(counter);
+
+    let quant = NeuQuant::new(10, 256, &colors);
     let palettized_imgs: Vec<Vec<u8>> = imgs.iter().map(|img| {
         img.pixels().map(|(_, _, px)| {
-            *palette.get(&px.to_rgba()).unwrap()
+            quant.index_of(&px.data) as u8
         }).collect()
     }).collect();
 
     GifDescriptor {
-        palette: compressed,
-        width: width,
-        height: height,
+        palette: quant.color_map_rgba(),
+        width: width as u16,
+        height: height as u16,
         images: palettized_imgs,
-    }
-}
-
-fn not_too_close(lab: Lab, sensitivity: f32, palette: &HashMap<Rgba<u8>, u8>, labs: &HashMap<Rgba<u8>, Lab>) -> bool {
-    palette.keys().map(|rgba| labs.get(rgba).unwrap()).all(|v| lab.squared_distance(&v) > sensitivity)
-}
-
-fn find_closest_rgb(px: &Rgba<u8>, palette: &mut HashMap<Rgba<u8>, u8>, labs: &mut HashMap<Rgba<u8>, Lab>) -> Rgba<u8> {
-    let lab_new = Lab::from_rgba(px.data);
-    let mut closest_rgb = None;
-    {
-        let mut closest_distance = std::f32::MAX;
-        for (rgb, lab) in palette.keys().map(|k| (k, labs.get(k).unwrap())) {
-            let dist = lab_new.squared_distance(lab);
-            if dist < closest_distance {
-                closest_distance = dist;
-                closest_rgb = Some(rgb.clone());
-            }
-        }
-    }
-    labs.insert(*px, lab_new);
-    closest_rgb.expect("Couldn't find ANY closest RGB colors?").clone()
-}
-
-fn compress_palette(colors: HashMap<Rgba<u8>, usize>) -> (HashMap<Rgba<u8>, u8>, HashMap<Rgba<u8>, u8>) {
-    let mut ctr: Vec<(Rgba<u8>, usize)> = colors.into_iter().collect();
-    ctr.sort_by(|a, b| b.1.cmp(&a.1));
-
-    let mut palette = HashMap::with_capacity(256);
-    let mut compressed_palette = palette.clone();
-    let mut lab_colors_by_rgb = HashMap::with_capacity(256);
-
-    if ctr.len() <= 256 {
-        for (i, (px, _)) in ctr.into_iter().enumerate() {
-            palette.insert(px, i as u8);
-            compressed_palette.insert(px, i as u8);
-        }
-    } else {
-        for (i, (px, _)) in ctr.into_iter().enumerate() {
-            if compressed_palette.len() < 256 && not_too_close(Lab::from_rgba(px.data), 300.0, &palette, &lab_colors_by_rgb) {
-                lab_colors_by_rgb.insert(px, Lab::from_rgba(px.data));
-                palette.insert(px, i as u8);
-                compressed_palette.insert(px, i as u8);
-            } else {
-                let closest_rgb = find_closest_rgb(&px, &mut compressed_palette, &mut lab_colors_by_rgb);
-                let i_new = *(compressed_palette.get(&closest_rgb).expect("Closest RGB color wasn't in the palette to begin with."));
-                palette.insert(px, i_new);
-                // whew!
-            }
-        }
-    }
-    (palette, compressed_palette)
-}
-
-fn count_colors(img: &DynamicImage, counter: &mut HashMap<Rgba<u8>, usize>) {
-    for (_, _, px) in img.pixels() {
-        let ctr = counter.entry(px).or_insert(0);
-        *ctr += 1;
     }
 }
