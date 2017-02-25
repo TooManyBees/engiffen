131261307f13ca788c2870714fada5dfce39fc9b
Jesse Bees <jesse@toomanybees.com>
2017-02-25 16:43:52 -0500

    Don't require manually remap rgba palette to rgb

--- src/lib.rs
+++ src/lib.rs
@@ -12,19 +12,9 @@ pub fn engiffen<W: Write>(imgs: &[DynamicImage], fps: usize, mut out: &mut W) {
     let gif_descriptor = palettize(&imgs);
     let delay = (1000 / fps) as u16;
 
-    let mut color_map: [u8; 256*3] = [0; 256*3];
-    let mut transparency = None;
-    for (idx, color) in gif_descriptor.palette.chunks(4).enumerate() {
-        color_map[idx*3] = color[0];
-        color_map[idx*3+1] = color[1];
-        color_map[idx*3+2] = color[2];
-        if color[3] == 0 {
-            transparency = Some(idx as u8);
-        }
-    }
     let width = gif_descriptor.width;
     let height = gif_descriptor.height;
-    let mut encoder = Encoder::new(&mut out, width, height, &color_map).unwrap();
+    let mut encoder = Encoder::new(&mut out, width, height, &gif_descriptor.palette).unwrap();
     encoder.set(Repeat::Infinite).unwrap();
     for img in gif_descriptor.images {
         let mut frame = Frame::default();
@@ -32,13 +22,14 @@ pub fn engiffen<W: Write>(imgs: &[DynamicImage], fps: usize, mut out: &mut W) {
         frame.width = width;
         frame.height = height;
         frame.buffer = Cow::Borrowed(&*img);
-        frame.transparent = transparency;
+        frame.transparent = gif_descriptor.transparency;
         encoder.write_frame(&frame).unwrap();
     }
 }
 
 struct GifDescriptor {
     palette: Vec<u8>,
+    transparency: Option<u8>,
     width: u16,
     height: u16,
     images: Vec<Vec<u8>>,
@@ -70,14 +61,18 @@ fn palettize(imgs: &[DynamicImage]) -> GifDescriptor {
     }
 
     let quant = NeuQuant::new(10, 256, &colors);
+    let mut transparency = None;
     let palettized_imgs: Vec<Vec<u8>> = imgs.iter().map(|img| {
         img.pixels().map(|(_, _, px)| {
-            quant.index_of(&px.data) as u8
+            let idx = quant.index_of(&px.data) as u8;
+            if px.data[3] == 0 { transparency = Some(idx); }
+            idx
         }).collect()
     }).collect();
 
     GifDescriptor {
-        palette: quant.color_map_rgba(),
+        palette: quant.color_map_rgb(),
+        transparency: transparency,
         width: width as u16,
         height: height as u16,
         images: palettized_imgs,
