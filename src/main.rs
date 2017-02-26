44755fdc8c42a2fe95c6eaa7a173341c2c58c88c
Jesse Bees <jesse@toomanybees.com>
2017-02-26 01:32:02 -0500

    Add some better error handling for binary

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,18 @@
 extern crate engiffen;
 extern crate image;
 extern crate getopts;
-// extern crate glob;
 
 use std::{env, error, fmt, process};
 use std::str::FromStr;
 use std::fs::{read_dir, File};
-use std::path::PathBuf;
-// use glob::glob;
+use std::path::{Path, PathBuf};
 use getopts::Options;
 
 use SourceImages::*;
 
 #[derive(Debug)]
 enum SourceImages {
-    StartEnd(String, String),
-    // Glob(String),
+    StartEnd(PathBuf, PathBuf, PathBuf),
     List(Vec<String>),
     StdIn,
 }
@@ -30,7 +27,6 @@ struct Args {
 #[derive(Debug)]
 enum ArgsError {
     Parse(getopts::Fail),
-    // Glob(glob::PatternError),
     Fps(std::num::ParseIntError),
     ImageRange(String),
 }
@@ -51,9 +47,8 @@ impl fmt::Display for ArgsError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             ArgsError::Parse(ref err) => write!(f, "Options parse error: {}", err),
-            // ArgsError::Glob(ref err) => write!(f, "Glob pattern error: {}", err),
             ArgsError::Fps(_) => write!(f, "Unable to parse framerate as an integer"),
-            ArgsError::ImageRange(ref s) => write!(f, "Incomplete range of images: {}", s),
+            ArgsError::ImageRange(ref s) => write!(f, "Bad image range: {}", s),
         }
     }
 }
@@ -62,19 +57,28 @@ impl error::Error for ArgsError {
     fn description(&self) -> &str {
         match *self {
             ArgsError::Parse(ref err) => err.description(),
-            // ArgsError::Glob(ref err) => err.description(),
             ArgsError::Fps(ref err) => err.description(),
-            ArgsError::ImageRange(_) => "Incomplete range of images",
+            ArgsError::ImageRange(_) => "Bad image range",
         }
     }
 
     fn cause(&self) -> Option<&error::Error> {
         match *self {
             ArgsError::Parse(ref err) => Some(err),
-            // ArgsError::Glob(ref err) => Some(err),
             ArgsError::Fps(ref err) => Some(err),
             ArgsError::ImageRange(_) => None,
         }
+    }
+}
+
+fn path_and_filename(input: &str) -> Result<(PathBuf, PathBuf), ArgsError> {
+    let p = Path::new(&input);
+    let parent = p.parent().unwrap_or(Path::new("."));
+    let filename = p.file_name();
+    if filename.is_none() {
+        Err(ArgsError::ImageRange(format!("Invalid filename {:?}", input)))
+    } else {
+        Ok((parent.to_owned(), PathBuf::from(filename.unwrap())))
     }
 }
 
@@ -90,7 +94,7 @@ fn parse_args() -> Result<Args, ArgsError> {
 
     let matches = try!{opts.parse(&args[1..])};
     if matches.opt_present("h") {
-        let brief = format!("Usage: {} <glob>", program);
+        let brief = format!("Usage: {} <files ...>", program);
         print!("{}", opts.usage(&brief));
         process::exit(0);
     }
@@ -104,11 +108,19 @@ fn parse_args() -> Result<Args, ArgsError> {
     let out_file = matches.opt_str("o").map(|f| f.clone()).unwrap_or("out.gif".to_string());
     let source = if matches.opt_present("r") {
         if matches.free.len() >= 2 {
-            StartEnd(matches.free[0].clone(), matches.free[1].clone())
+            let (path_start, filename_start) = path_and_filename(&matches.free[0])?;
+            let (path_end, filename_end) = path_and_filename(&matches.free[1])?;
+            if path_start != path_end {
+                return Err(ArgsError::ImageRange("start and end files are from different directories".to_string()))
+            }
+            if !path_start.exists() {
+                return Err(ArgsError::ImageRange(format!("directory not readable: {:?}", path_start)))
+            }
+            StartEnd(path_start, filename_start, filename_end)
         } else if matches.free.len() == 1 {
-            return Err(ArgsError::ImageRange("end filename".to_string()))
+            return Err(ArgsError::ImageRange("missing end filename".to_string()))
         } else {
-            return Err(ArgsError::ImageRange("start and end filenames".to_string()))
+            return Err(ArgsError::ImageRange("missing start and end filenames".to_string()))
         }
     } else if matches.free.is_empty() {
         StdIn
@@ -129,19 +141,8 @@ fn main() {
         process::exit(1);
     }).unwrap();
     let source_images: Vec<PathBuf> = match args.source {
-        // Glob(pattern) => {
-        //     // already tested pattern validity in parse_args()
-        //     glob(&pattern).unwrap()
-        //     .filter(|e| {
-        //         e.is_ok()
-        //     })
-        //     .map(|e| e.unwrap())
-        //     .collect()
-        // },
-        StartEnd(start_pattern, end_pattern) => {
-            let start_path = PathBuf::from(&start_pattern);
-            let end_path = PathBuf::from(&end_pattern);
-            read_dir(".").unwrap()
+        StartEnd(dir, start_path, end_path) => {
+            read_dir(dir).unwrap()
             .map(|e| e.unwrap().path())
             .skip_while(|path| path.file_name().unwrap() < start_path)
             .take_while(|path| path.file_name().unwrap() <= end_path)
